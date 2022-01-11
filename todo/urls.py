ea9b5727be400eb1d09b47ae0c43ca715feb4a39
trevennue <trevepaul2@gmail.com>
2022-01-11 07:22:04 +0000

    First working version of the todo app done

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,10 @@ from . import views as todo_views
 
 urlpatterns = [
     path("", todo_views.home, name="todo-home"),
+    path("add-todo-item/", todo_views.add_todo_item, name="add-todo-item"),
+    path(
+        "delete-todo-item/<int:todo_item_id>/",
+        todo_views.delete_todo_item,
+        name="delete-todo-item",
+    ),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import TodoItem
 
 
 def home(request):
-    context = {}
+    context = {
+        "todo_items": TodoItem.objects.all(),
+    }
     return render(request, "todo/home.html", context=context)
+
+
+def add_todo_item(request):
+    content = request.POST["content"]
+    new_todo_item = TodoItem(content=content)
+    new_todo_item.save()
+    return redirect("todo-home")
+
+
+def delete_todo_item(request, todo_item_id):
+    todo_item = TodoItem(id=todo_item_id)
+    todo_item.delete()
+    return redirect("todo-home")
